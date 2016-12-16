9002cd8876c900339f3140fc7e4e7437b8bc48c6
Krzysztof <krzysztof@bialoglowicz.pl>
2016-12-16 22:11:12 +0100

    fix motiondetect

--- motiondetect.py
+++ motiondetect.py
@@ -4,20 +4,18 @@ import time #for sleep/pause
 import RPi.GPIO as io #read the GPIO pins
 
 io.setmode(io.BCM)
-
-pir_pin = 17 # Sensor pin 25 for data out of the PIR sensor 
+pir_pin = 17
+screen_saver = False
 
 io.setup(pir_pin, io.IN)
-print("Motion Activated (PIR) Screen Saver")
-print("less than 30 seconds to leave the sensor area...")
-time.sleep(2)
-os.system("xscreensaver-command -activate") #OS command for Xscreensaver program = Activate, turns on screensaver instantly
 
-while True:  # endless loop, waiting for nothing. Use Control+C to exit
-	if io.input(pir_pin): # Waits for motion signal from PIR on GPIO pin 25, then com[plete rest and exit
-		os.system("xscreensaver-command -deactivate") ##OS command for Xscreensaver program = Activate, turns on screensaver instantly
-		print("PIR Screen Saver Deactivated")
-		io.cleanup() #reset GPIO
-		sys.exit() #exit cleanly to the command prompt.
-	
+while True:  
+	if screen_saver:
+		if io.input(pir_pin): 
+			os.system("xscreensaver-command -deactivate")
+			screen_saver = False
+	else:
+		time.sleep(2)
+		os.system("xscreensaver-command -activate")
+		screen_saver = True 
 
